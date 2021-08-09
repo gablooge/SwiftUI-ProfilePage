9048436e7d3c795c9e8ef800c8a7835d7fe5103b
Samsul Hadi <samsulhadikediri@gmail.com>
2021-08-09 14:12:08 +0700

    Initial Commit

---
+++ Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Samsul Hadi on 09/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
